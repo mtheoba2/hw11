85f3b5fa8e20fb9090a0ed226c165b91f99672e4
mtheoba2 <105365746+mtheoba2@users.noreply.github.com>
2023-01-06 14:03:23 +0100

    Update main.java

--- main.java
+++ main.java
@@ -2,9 +2,6 @@ public class Main {
 
     public static void main(String [] args) {
         System.out.println("Code at line 4");
-<<<<<<< HEAD
         System.out.println("Code at line 5 added in Branch_1");
-=======
-
     }
 }
